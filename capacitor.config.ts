b1d1447b4c5051510cb0764ed4457e65d821db7a
Christian2-do <candrango@estud.tesa.edu.ec>
2026-02-16 21:44:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'primera.web',
+  appName: 'primera-web',
+  webDir: 'dist'
+};
+
+export default config;
